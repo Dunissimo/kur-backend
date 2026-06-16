db4a27405e0fa3c8308fdeb71390970fb648f0f3
Dunissimo <SuperDen214@yandex.ru>
2026-06-16 08:18:28 +0400

    d

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 const server = express();
 
 export async function bootstrap() {
     const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-    app.enableCors({
+    const corsOptions: CorsOptions = {
         origin: '*',
-    });
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    };
+
+        app.enableCors(corsOptions);
 
     if (process.env.NODE_ENV !== 'production') {
         await app.listen(3000);
